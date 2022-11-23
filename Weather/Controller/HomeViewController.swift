b1a05975414a7ecb99e43c961a2b0c8878afc044
Hakkı Can Şengönül <coder.9734@gmail.com>
2022-11-23 09:39:01 +0300

    Setup

---
+++ Weather/Controller/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by hakkı can şengönül on 23.11.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+     // MARK: - Properties
+    private let backgroundImageView = UIImageView()
+     // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+ // MARK: - Helpers
+extension HomeViewController{
+    private func style(){
+        //backgroundImageView style
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.image = #imageLiteral(resourceName: "background")
+        
+    }
+    private func layout(){
+        view.addSubview(backgroundImageView)
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: backgroundImageView.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: backgroundImageView.bottomAnchor)
+        ])
+        
+    }
+}
+
